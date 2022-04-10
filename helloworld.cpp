b9ddfdfc962486f55bea1f41649044a49861be27
kommutator <kommutator@yandex.ru>
2022-04-11 01:40:02 +0300

    a+b, branch newfeature

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <cstdlib> // для system
 using namespace std;
+unsigned int a;
+unsigned int b;
+unsigned int c;
 
 int main()
 {
-    cout << "Hello, world!" << endl;
+	a = 2;
+	b = 3;
+	c = a + b;          
+
+    cout << "Hello, world! "<< c << endl;
     system("pause"); // Только для тех, у кого MS Visual Studio
     return 0;
 }
