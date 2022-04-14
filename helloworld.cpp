13f7cd97ba0bebfb380de0c50993f27d78cb49bd
kommutator <kommutator@yandex.ru>
2022-04-15 01:03:13 +0300

    test class

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,9 +4,21 @@ using namespace std;
 unsigned int a;
 unsigned int b;
 unsigned int c;
+class myclass
+{
+	public:
+	 int s,f;
+
+};
+
 
 int main()
 {
+	myclass mc;
+
+	mc.s = 5;
+	mc.f = 7;
+
 	a = 2;
 	b = 3;
 	c = a + b;          
